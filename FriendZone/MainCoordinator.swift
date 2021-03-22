914875495629affd1290225b3cefb1462649cd53
iosdeveloperashish <iosdeveloperashish@gmail.com>
2021-03-22 22:44:44 +0530

    Removed space

--- FriendZone/MainCoordinator.swift
+++ FriendZone/MainCoordinator.swift
@@ -13,7 +13,6 @@ class MainCoordinator: Coordinator {
     
     var navigationController: UINavigationController
     
-    
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
